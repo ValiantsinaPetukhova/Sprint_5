3daec29ca20fed0b920a226dccb862fe2d286577
Valentina Petuhova <valiantsinapetuhova@yandex.ru>
2024-03-11 12:34:33 +0300

    Sprint 5 fixed: login in constructor tests was removed

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -5,17 +5,7 @@ from locators import *
 
 
 class TestConstructor:
-    def test_switch_to_buns(self, new_user_registration, driver):
-        email = new_user_registration[0]
-        password = new_user_registration[1]
-        # Login
-        driver.find_element(By.XPATH, LOGIN_ACCOUNT_BUTTON).click()
-        driver.find_element(By.CSS_SELECTOR, LOGIN_INPUT).send_keys(email)
-        driver.find_element(By.CSS_SELECTOR, PASSWORD_INPUT).send_keys(password)
-        driver.find_element(By.XPATH, LOGIN_BUTTON).click()
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, BUNS_SECTION)))
-
+    def test_switch_to_buns(self, driver):
         # Переключаемся на вкладку "Соусы", а затем возвращаемся на "Булки"
         sauces_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, SAUCES_SECTION)))
         sauces_element.click()
@@ -30,16 +20,7 @@ class TestConstructor:
         element_buns = driver.find_element(By.XPATH, SOME_BUNS)
         assert element_buns.is_displayed()
 
-    def test_switch_to_sauces(self, new_user_registration, driver):
-        email = new_user_registration[0]
-        password = new_user_registration[1]
-        # Login
-        driver.find_element(By.XPATH, LOGIN_ACCOUNT_BUTTON).click()
-        driver.find_element(By.CSS_SELECTOR, LOGIN_INPUT).send_keys(email)
-        driver.find_element(By.CSS_SELECTOR, PASSWORD_INPUT).send_keys(password)
-        driver.find_element(By.XPATH, LOGIN_BUTTON).click()
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, SAUCES_SECTION)))
+    def test_switch_to_sauces(self, driver):
 
         # Переключаемся на вкладку "Соусы"
         sauces_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, SAUCES_SECTION)))
@@ -48,17 +29,7 @@ class TestConstructor:
         element_sauce = driver.find_element(By.XPATH, SOME_SAUCE)
         assert element_sauce.is_displayed()
 
-    def test_switch_to_fillings(self, new_user_registration, driver):
-        email = new_user_registration[0]
-        password = new_user_registration[1]
-        # Login
-        driver.find_element(By.XPATH, LOGIN_ACCOUNT_BUTTON).click()
-        driver.find_element(By.CSS_SELECTOR, LOGIN_INPUT).send_keys(email)
-        driver.find_element(By.CSS_SELECTOR, PASSWORD_INPUT).send_keys(password)
-        driver.find_element(By.XPATH, LOGIN_BUTTON).click()
-        WebDriverWait(driver, 10).until(
-            EC.visibility_of_element_located((By.XPATH, FILLINGS_SECTION)))
-
+    def test_switch_to_fillings(self, driver):
         # Переключаемся на вкладку "Начинки"
         fillings_element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, FILLINGS_SECTION)))
         fillings_element.click()
